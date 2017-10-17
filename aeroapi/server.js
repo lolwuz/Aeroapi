a2f11c228aa0813da20ce06f5501eb3abbb2be48
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-17 06:25:25 -0700

    Server

--- aeroapi/server.js
+++ aeroapi/server.js
@@ -14,6 +14,13 @@ mongoose.connect('mongodb://localhost:27017/airliner');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 var airlinerRoutes = require('./api/routes/airlinerRoutes'); //importing route
 airlinerRoutes(app); //register the route
 
