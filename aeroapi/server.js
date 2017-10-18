ca25398001b7a83fda7dd781cb79666fffdaf9aa
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-18 11:26:34 +0200

    Basic routes revert

--- aeroapi/server.js
+++ aeroapi/server.js
@@ -2,24 +2,17 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
-morgan = require('morgan'),
 Airliner = require('./api/models/airlinerModel'), //created model loading here
 Airport = require('./api/models/airportModel'), //created model loading here
-<<<<<<< HEAD
 Plane = require('./api/models/planeModel'), //created model loading here
-docs = require("express-mongoose-docs");
-
-=======
+docs = require("express-mongoose-docs"),
 Plane = require('./api/models/planeModel'),
-User = require('./api/routes/userRoutes'), //created model loading here
->>>>>>> 0c75454498219368118f973de261af20745ef396
 bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/airliner'); 
 
-app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -29,9 +22,6 @@ app.use(function(req, res, next) {
     next();
   });
   
-
-app.use('/users', routes);
-
 var airlinerRoutes = require('./api/routes/airlinerRoutes'); //importing route
 airlinerRoutes(app); //register the route
 
