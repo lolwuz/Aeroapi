f2e1e3af7e266d4adb755228309246b3f7e89f04
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-18 14:02:10 +0200

    Route controll

--- aeroapi/api/routes/routeRoutes.js
+++ aeroapi/api/routes/routeRoutes.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function(app) {
   var route = require('../controllers/routeController');
+
+  // todoList Routes
+  app.route('/route')
+  .get(airport.list_all_route)
+  .post(airport.create_a_route)
+
+app.route('/route/:airportId')
+  .get(airport.read_a_route)
+  .put(airport.update_a_route)
+  .delete(airport.delete_a_route);
 };
